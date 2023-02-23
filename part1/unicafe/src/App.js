09dfbe8aabbb105f37cd6ae532afcabbdf58d77b
William Crockett <willcrockett4@gmail.com>
2023-02-22 17:52:02 -0800

    finished unicafe exercises (1.6-1.11)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,20 +17,40 @@ const App = () => {
       <Button onClick={handleNeutralClick} text='Neutral'/>
       <Button onClick={handleBadClick} text='Bad'/>
       <Header text='Statistics'/>
-      <Stats good={good} neutral={neutral} bad={bad}/>
-      <p></p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
 
 const Header = ({text}) => <h1>{text}</h1>
-const Stats = ({good, neutral, bad}) => {
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const average = (good - bad)/total
+  const percentPos =  '' + (good/total)*100 + ' %'
+  if (total > 0) {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine value={good} text='Good'/>
+          <StatisticLine value={neutral} text='Neutral'/>
+          <StatisticLine value={bad} text='Bad'/>
+          <StatisticLine value={total} text='Total'/>
+          <StatisticLine value={average} text='Average'/>
+          <StatisticLine value={percentPos} text='Percent positive'/>
+        </tbody>
+      </table>
+    )
+  }
+  return <p>No feedback given yet</p>
+}
+
+const StatisticLine = ({value, text}) => {
   return (
-    <div>
-      <li>Good {good}</li>
-      <li>Neutral {neutral}</li>
-      <li>Bad {bad}</li>
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 const Button = ({onClick, text}) =>  <button onClick={onClick}>{text}</button>
