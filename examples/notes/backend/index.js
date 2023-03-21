0d2ccd5dd82684d284a9528b8a7d9b60e3fa97cf
William Crockett <willcrockett4@gmail.com>
2023-03-20 20:46:41 -0700

    changed note app post new note and fetch all note routes to work with mongodb

--- examples/notes/backend/index.js
+++ examples/notes/backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const Note = require('./models/Note')
+const Note = require('./models/note')
 const cors = require('cors')
 /* -------------------------------------------------------------------------- */
 /*                                 Middleware                                 */
@@ -66,23 +66,13 @@ app.get('/api/notes/', (request, response) => {
 
 /* ---------------------------- GET Specific note --------------------------- */
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
-  })
-  console.log(note)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else { // handle attempted retrieval of nonexistent note
-    response.status(404).end()
-    // end() responds to request without attaching any data
-  }
+  })
 })
 /* ------------------------------- DELETE Note ------------------------------ */
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(n => n.id !== id)
+  Note.
 
   response.status(204).end() /* no clear precendent for status code for failed delete
                               only two options 204 and 404. using 204 for F&S for simpliciity */
@@ -90,10 +80,18 @@ app.delete('/api/notes/:id', (request, response) => {
 
 /* ------------------------------ POST new note ----------------------------- */
 app.post('/api/notes', (request, response) => {
-  const note = request.body // only possible because of json-parser. body would be undefined otherwise
-  console.log(note)
-  response.json(note)
-  
+  const body = request.body 
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => response.json(savedNote))
+
 })
 
 app.use(unknownEndpoint)
