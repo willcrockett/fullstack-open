7a2d8c462ce7fe814c98e5c722ebd59a08f2a843
William Crockett <willcrockett4@gmail.com>
2023-03-21 20:25:15 -0700

    added update route to phonebook

--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -19,31 +19,18 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms \n
 app.use(express.static('build'))
 
 
-/* ---------------------------- Helpers and Data ---------------------------- */
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+/* ---------------------------- Controllers ---------------------------- */
+const update = (req, res, next) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
   }
-]
 
-const generateId = () => Math.floor(Math.random() * 10000)
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(error => next(error))
+}
+
 /* -------------------------------------------------------------------------- */
 /*                               RESTful Routes                               */
 /* -------------------------------------------------------------------------- */
@@ -87,9 +74,7 @@ app.post('/api/persons', (req, res) => {
       error: 'number missing'
     })
   } else if (persons.some(p => p.name.toUpperCase() == body.name.toUpperCase())) {
-    return res.status(400).json({
-      error: 'Name must be unique'
-    })
+    app.put('/api/persons/:id', update)
   }
 
   const person = new Person({
@@ -104,6 +89,7 @@ app.post('/api/persons', (req, res) => {
         })
 })
 
+app.put('/api/persons/:id', update)
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
