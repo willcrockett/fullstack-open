00b95fb95690db840b6ce4e6a9d25a6e88f2b075
William Crockett <willcrockett4@gmail.com>
2023-03-27 20:44:23 -0700

    finsiehd 4.2 exericse

---
+++ part4/bloglist/bloglist-backend/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
