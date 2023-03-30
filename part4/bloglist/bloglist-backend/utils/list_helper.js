f064c49df19d710cb96a2672afeeacb50b55abed
William Crockett <willcrockett4@gmail.com>
2023-03-29 19:52:12 -0700

    finished 4.5

--- part4/bloglist/bloglist-backend/utils/list_helper.js
+++ part4/bloglist/bloglist-backend/utils/list_helper.js
@@ -1,9 +1,25 @@
 const totalLikes = (blogs) => {
 	return blogs.reduce(
-		(acc, currBlog, ) => acc + currBlog.likes, 0
-	)
+		(acc, currBlog) => {
+			return acc + currBlog.likes
+		}, 0)
 }
 
+/**
+ * Finds out which blog has the most likes.
+ *  If there are many top favorites, it is enough to return one of them.
+ */
+const favoriteBlog = (blogs) => {
+
+	return (
+		(({title, author, likes}) => ({title, author, likes}))(  // destructuring
+			blogs.reduce( 
+				(max, currBlog) => currBlog.likes > max.likes ? currBlog : max
+			))
+	)
+	
+}
 module.exports = {
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
