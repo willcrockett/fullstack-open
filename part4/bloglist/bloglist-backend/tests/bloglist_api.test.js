d7bc11a44d8a545d00ed73c9b4cdc8979091b81e
William Crockett <willcrockett4@gmail.com>
2023-04-03 20:08:33 -0700

    finshed 4.9: passing uid naming verification test

--- part4/bloglist/bloglist-backend/tests/bloglist_api.test.js
+++ part4/bloglist/bloglist-backend/tests/bloglist_api.test.js
@@ -24,4 +24,17 @@ describe('when there is initially some saved blogs', () => {
 
 		expect(res.body).toHaveLength(helper.initialBlogs.length)
 	})
+
+	test('blog uid property named id', async () => {
+		const res = await api.get('/api/blogs')
+
+		res.body.forEach((blog) => {
+			expect(blog.id).toBeDefined()
+			expect(blog._id).not.toBeDefined()
+		})
+	})
+
+	afterAll(async () => {
+		await mongoose.connection.close()
+	})
 })
